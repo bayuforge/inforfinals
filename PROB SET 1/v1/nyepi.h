a6499043e25c11cac8ef95193d9f30bcb5875340
bayuforge <bayuanataw@gmail.com>
2026-03-28 19:39:51 +0800

    Add header file

---
+++ PROB SET 1/v1/nyepi.h
@@ -0,0 +1,15 @@
+#ifndef NYEPI
+#define NYEPI
+
+struct ogoh{
+        bool og1;
+        bool og2;
+        bool og3;
+        bool og4;
+};
+
+void preprocessing(bool a1,bool a2,bool a3,bool a4);
+void process(ogoh* o);
+void output_data(ogoh* o,float hasil);
+
+#endif
