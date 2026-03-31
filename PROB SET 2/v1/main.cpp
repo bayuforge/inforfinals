e05f3f58f8b4be3de765521f8774d2ba32fe6662
bayuforge <bayuanataw@gmail.com>
2026-03-31 23:52:58 +0800

    Create main.cpp with usage notice

---
+++ PROB SET 2/v1/main.cpp
@@ -0,0 +1,16 @@
+/* NOTICE (must read for anyone following my code entirely)
+  if you follow my code, iostream are optional. You can comment them. The reason I added it because the exam style 
+  one was a mandatory requirement. It acts as a safety guard so you don't actually makes the code error in compilation
+  because some people actually puts it without having input output at their 'main.cpp' code. So having to know the 
+  reason why you're using them are very important knowledge to know. So you don't just blindly follow some instruction.
+*/
+
+#include <iostream> 
+#include "nyepi.h"
+using std::cout;
+
+int main(){
+        input();
+
+        return 0;
+}
